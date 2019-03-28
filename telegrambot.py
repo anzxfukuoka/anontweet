fa3e7798aa1e167ece7c9e0c162b0f4d21608861
Anzx <0.love.2d.atsi.2@gmail.com>
2019-03-28 15:56:44 +0200

    третий ᕕ( ᐛ )ᕗ

--- telegrambot.py
+++ telegrambot.py
@@ -1,9 +1,11 @@
 import config
 import tweetbot
 import telebot
+from flask import Flask, request
 
 bot = telebot.TeleBot(config.telegram_token)
 
+server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -36,4 +38,21 @@ def send_tweet(message):
 	bot.send_message(message.chat.id, tweetbot.sendtweet(message.text))
 
 
-bot.polling()
+@server.route('/' + token, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://anontweet.herokuapp.com/' + config.token)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    pass
+
+#bot.polling()
