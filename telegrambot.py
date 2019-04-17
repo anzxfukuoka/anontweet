e6688f4cca7b97ef417a8486e97c89de52ed727d
Anzx <0.love.2d.atsi.2@gmail.com>
2019-04-18 00:14:36 +0300

    text > 280 chars allowed

--- telegrambot.py
+++ telegrambot.py
@@ -58,10 +58,10 @@ def webhook():
 
 
 if __name__ == "__main__":
-    #server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
     pass
 
 
 #dbg
-bot.remove_webhook()
-bot.polling()
+#bot.remove_webhook()
+#bot.polling()
